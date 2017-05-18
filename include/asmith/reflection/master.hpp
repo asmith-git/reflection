3f1a81d4e04e4cdd18f6105a5bcb793bf86846f8
Adam Smith <adam.george.smith@btinternet.com>
2017-05-18 12:48:31 +0100

    Master header

---
+++ include/asmith/reflection/master.hpp
@@ -0,0 +1,24 @@
+//	Copyright 2017 Adam Smith
+//	Licensed under the Apache License, Version 2.0 (the "License");
+//	you may not use this file except in compliance with the License.
+//	You may obtain a copy of the License at
+// 
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+//	Unless required by applicable law or agreed to in writing, software
+//	distributed under the License is distributed on an "AS IS" BASIS,
+//	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//	See the License for the specific language governing permissions and
+//	limitations under the License.
+
+#ifndef ASMITH_REFLECTION_MASTER_HPP
+#define ASMITH_REFLECTION_MASTER_HPP
+
+#include "reflection_class.hpp"
+#include "reflection_constructor.hpp"
+#include "reflection_destructor.hpp"
+#include "reflection_function.hpp"
+#include "reflection_variable.hpp"
+#include "reflection_modifier.hpp"
+
+#endif
