b8106245d436a5800f70a68762056a2bc7313bc7
Adam Smith <adam.george.smith@btinternet.com>
2017-05-18 15:54:18 +0100

    Reflection example

--- include/asmith/reflection/master.hpp
+++ include/asmith/reflection/master.hpp
@@ -21,4 +21,38 @@
 #include "reflection_variable.hpp"
 #include "reflection_modifier.hpp"
 
+namespace asmith {
+	struct reflection_test {
+		std::string name;
+		int dimensions;
+		float lower_bound;
+		float upper_bound;
+
+		#define ASMITH_REFLECTION_CLASS_NAME reflection_test
+		#define ASMITH_REFLECTION_VARIABLE_COUNT 4
+		#define ASMITH_REFLECTION_CONSTRUCTOR_COUNT 0
+		#define ASMITH_REFLECTION_FUNCTION_COUNT 0
+		#define ASMITH_REFLECTION_PARENT_COUNT 0
+		ASMITH_BEGIN_REFLECTION
+			const asmith::reflection_function& get_function(size_t) const override { throw 0; }
+			const asmith::reflection_constructor& get_constructor(size_t) const override { throw 0; }
+			const asmith::reflection_destructor& get_destructor() const override { throw 0; }
+			const reflection_class& get_parent_class(size_t) const override { throw 0; }
+
+			ASMITH_BEGIN_REFLECTION_VARIABLES
+				ASMITH_REFLECTION_VARIABLE(0, name,			REFLECTION_PUBLIC);
+				ASMITH_REFLECTION_VARIABLE(1, dimensions,	REFLECTION_PUBLIC);
+				ASMITH_REFLECTION_VARIABLE(2, lower_bound,	REFLECTION_PUBLIC);
+				ASMITH_REFLECTION_VARIABLE(3, upper_bound,	REFLECTION_PUBLIC);
+			ASMITH_END_REFLECTION_VARIABLES
+		ASMITH_END_REFLECTION
+
+		#undef ASMITH_REFLECTION_CLASS_NAME
+		#undef ASMITH_REFLECTION_VARIABLE_COUNT
+		#undef ASMITH_REFLECTION_CONSTRUCTOR_COUNT
+		#undef ASMITH_REFLECTION_FUNCTION_COUNT
+		#undef ASMITH_REFLECTION_PARENT_COUNT
+	};
+}
+
 #endif
