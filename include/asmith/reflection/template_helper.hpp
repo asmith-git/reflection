fb0fecd613b07a423b8d9f7e3e19063485320b2f
Adam Smith <adam.george.smith@btinternet.com>
2017-05-24 12:38:06 +0100

    Type lists

--- include/asmith/reflection/template_helper.hpp
+++ include/asmith/reflection/template_helper.hpp
@@ -19,6 +19,9 @@
 #define ASMITH_REFLECTION_TEMPLATE_HELPER_HPP
 
 namespace asmith {
+
+	// Constant value comparison
+
 	template<class T, T A, T B>
 	struct template_value_comparison {
 		typedef T type;
@@ -32,20 +35,69 @@ namespace asmith {
 		};
 	};
 
-	template<size_t I, class ...TYPES>
-	class type_at_index {
-	private:
-		template<size_t I, bool CHECK, class ...TYPES>
-		struct type_at_index_ {
-			typedef decltype(std::get<I>(std::tuple<TYPES...>())) type;
+	// Type lists
+
+	struct type_list_end {
+		enum {
+			SIZE = 0
+		};
+	};
+
+	namespace implementation {
+		template<class H, class T>
+		struct type_list_ {
+			typedef H head_t;
+			typedef T tail_t;
+
+			enum {
+				SIZE = 1 + T::SIZE
+			};
 		};
 
-		template<size_t I, class ...TYPES>
-		struct type_at_index_<I, false, TYPES...> {
+		template<size_t I, class H, class ...TYPES>
+		struct type_list__ {
+			typedef type_list_<H,typename type_list__<I-1, TYPES...>::type> type;
+		};
+
+		template<class H, class ...TYPES>
+		struct type_list__<1, H, TYPES...> {
+			typedef type_list_<H, type_list_end> type;
+		};
+	}
+
+	template<class ...TYPES>
+	using type_list = typename implementation::type_list__<sizeof...(TYPES), TYPES...>::type;
+
+	namespace implementation {
+		template<size_t I, class LIST, class ENABLE = void>
+		struct type_list_index_ {
+			typedef typename type_list_index_<I-1, typename LIST::tail_t>::type type;
+		};
+
+		template<class LIST>
+		struct type_list_index_<0, LIST> {
+			typedef LIST type;
+		};
+	}
+
+	template<size_t I, class LIST>
+	using type_list_index = typename implementation::type_list_index_<I, LIST>::type;
+
+	// Varadic template access
+
+	namespace implementation {
+		template<size_t I, class LIST, class ENABLE_ = void>
+		struct type_at_index_ {
+			typedef typename type_list_index<I, LIST>::head_t type;
+		};
+
+		template<size_t I, class LIST>
+		struct type_at_index_<I, LIST, typename std::enable_if<template_value_comparison<size_t, I, LIST::SIZE>::GREATER_EQUAL>::type> {
 			typedef void type;
 		};
-	public:
-		typedef typename type_at_index_<I, template_value_comparison<size_t, I, sizeof...(TYPES)>::LESS, TYPES...>::type type;
-	};
+	}
+
+	template<size_t I, class ...TYPES>
+	using type_at_index = typename implementation::type_at_index_<I, type_list<TYPES...>>::type;
 }
 #endif
